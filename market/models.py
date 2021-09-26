79404af8da229f45bfa84c39c10dabf798a4e669
Mikhail Shevchenko <m.shevchenko@magdv.com>
2021-09-26 22:52:14 +0700

    feat(database): add flask sqlalchemy db

---
+++ market/models.py
@@ -0,0 +1,10 @@
+
+class Item(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(length=30), nullable=False, unique=False)
+    price = db.Column(db.Integer(), nullable=False)
+    barcode = db.Column(db.String(length=12), nullable=False, unique=True)
+    description = db.Column(db.String(length=1024), nullable=True, unique=False)
+
+    def __repr__(self):
+        return f'Item {self.name}'
