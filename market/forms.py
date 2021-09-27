adb3496c48e782b97ac3e856c7f2386c4456e285
Mikhail Shevchenko <m.shevchenko@magdv.com>
2021-09-27 21:39:46 +0700

    feat: add validations on user register

--- market/forms.py
+++ market/forms.py
@@ -1,10 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import Length, EqualTo, Email, DataRequired
 
 
 class RegisterForm(FlaskForm):
-    username = StringField(label='')
-    email_address = StringField(label='')
-    password1 = PasswordField(label='')
-    password2 = PasswordField(label='')
+    username = StringField(label='', validators=[Length(min=2, max=30), DataRequired()])
+    email_address = StringField(label='', validators=[Email(), DataRequired()])
+    password1 = PasswordField(label='', validators=[Length(min=6), DataRequired()])
+    password2 = PasswordField(label='', validators=[EqualTo('password1'), DataRequired()])
     submit = SubmitField(label='Create Account')
--- market/routes.py
+++ market/routes.py
@@ -1,7 +1,9 @@
 from market import app
-from flask import render_template
+from market import db
+from flask import render_template, redirect, url_for, flash
 from market.models import Item, User
 from market.forms import RegisterForm
+
 
 @app.route('/')
 @app.route('/home')
@@ -15,10 +17,20 @@ def market_page():
 
     return render_template('market.html', items=items)
 
-@app.route('/register')
+
+@app.route('/register', methods=['GET', 'POST'])
 def register_page():
     form = RegisterForm()
     if form.validate_on_submit():
-        user_to_create = User()
+        user_to_create = User(username=form.username.data,
+                              email_address=form.email_address.data,
+                              password_hash=form.password1.data)
 
+        db.session.add(user_to_create)
+        db.session.commit()
+
+        return redirect(url_for('market_page'))
+    if form.errors != {}:
+        for err_msg in form.errors.values():
+            flash(f'There was an error with creating user: {err_msg}')
     return render_template('register.html', form=form)
