ebe599c28222fa780c3a85ce1b0339559fc2bf7a
Mikhail Shevchenko <m.shevchenko@magdv.com>
2021-09-27 22:06:52 +0700

    chore: add existing username and rmail validation

--- market/forms.py
+++ market/forms.py
@@ -1,9 +1,23 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import Length, EqualTo, Email, DataRequired
+from wtforms.validators import Length, EqualTo, Email, DataRequired, ValidationError
+from market.models import User
 
 
 class RegisterForm(FlaskForm):
+
+    def validate_username(self, username):
+
+        user = User.query.filter_by(username=username.data).first()
+
+        if user:
+            raise ValidationError('Username already exist')
+
+    def validate_email_address(self, email_address_to_check):
+        email_address = User.query.filter_by(email_address=email_address_to_check.data).first()
+        if email_address:
+            raise ValidationError('Email Address already exists! Please try a different email address')
+
     username = StringField(label='', validators=[Length(min=2, max=30), DataRequired()])
     email_address = StringField(label='', validators=[Email(), DataRequired()])
     password1 = PasswordField(label='', validators=[Length(min=6), DataRequired()])
--- market/routes.py
+++ market/routes.py
@@ -32,5 +32,5 @@ def register_page():
         return redirect(url_for('market_page'))
     if form.errors != {}:
         for err_msg in form.errors.values():
-            flash(f'There was an error with creating user: {err_msg}')
+            flash(f'There was an error with creating user: {err_msg}', category='danger')
     return render_template('register.html', form=form)
